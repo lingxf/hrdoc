f2d83db1376a5d430e256a341543dd9cc096ccf0
Xiaofeng(Daniel) Ling <xling@qti.qualcomm.com>
2017-07-24 11:56:22 +0800

    fix bug of role after change filter

--- show_document.php
+++ show_document.php
@@ -7,14 +7,12 @@
 include 'myphp/common.php';
 include 'myphp/disp_lib.php';
 include 'hrdoc_lib.php';
+include 'hrdoc_records.php';
 include 'debug.php';
 include 'db_connect.php';
 
 session_name('hrdoc');
 session_start();
-
-if(isset($_GET['view'])) $view=$_GET['view'];
-else $view=$_SESSION['view'];
 
 $doctype = get_persist_var('doctype', -1);
 $order = get_persist_var('order', 0);
@@ -25,13 +23,13 @@ $create_date = get_persist_var('create_date', -1);
 $uid = get_persist_var('uid', '');
 	
 $login_id = get_session_var('user', 'Guest');
-#$role = is_member($login_id);
+$role = get_member_role($login_id);
 
 $start=$_SESSION['start'];
 $start=0;
 $items_perpage=$_SESSION['items_perpage'];
 
 $cond = get_cond_from_var($doctype, $status, $uid, $room, $submitter, $create_date);
-list_document($view, 0, $start, $items_perpage, $cond ,$order);
+list_document($start, $items_perpage, $cond ,$order);
 
 ?>
